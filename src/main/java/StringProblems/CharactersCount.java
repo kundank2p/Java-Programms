4cd50ced2756b11131bb9cd38b876e6fb8e07670
Kundan <140997405+kundank2p@users.noreply.github.com>
2025-01-31 15:41:35 +0530

    Update CharactersCount.java

--- src/main/java/StringProblems/CharactersCount.java
+++ src/main/java/StringProblems/CharactersCount.java
@@ -1,9 +1,15 @@
 package StringProblems;
+
 public class CharactersCount {
+    
     public static void main(String[] args) {
+        
         String input = "JAVA is Amazing";
+        
         long count = input.chars().filter(ch -> ch == 'A' || ch == 'a').count();
+        
        long  count1 = input.chars().filter(ch->ch=='A'|| ch == 'a').count();
+        
         System.out.println("Number of 'A's: " + count1);
 
     }
