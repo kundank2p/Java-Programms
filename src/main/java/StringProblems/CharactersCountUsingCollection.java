2515dd626ee0111c795760dc8a885492c1c02def
Kundan <140997405+kundank2p@users.noreply.github.com>
2025-01-31 15:42:12 +0530

    Update CharactersCountUsingCollection.java

--- src/main/java/StringProblems/CharactersCountUsingCollection.java
+++ src/main/java/StringProblems/CharactersCountUsingCollection.java
@@ -2,15 +2,18 @@ package StringProblems;
 
 import java.util.HashMap;
 
-public class CharactersCountUsingCollection
-{
+public class CharactersCountUsingCollection{
+    
     public static void main(String[] args) {
+        
         String input = "MADAM";
         HashMap<Character,Integer> frequencyMap = new HashMap<Character,Integer>();
         char[] inputArray = input.toCharArray();
+        
         for (char x : inputArray ) {
             frequencyMap.put(x, frequencyMap.getOrDefault(x, 0) + 1);
         }
+        
         for (char c : frequencyMap.keySet()){
             System.out.println(c+" "+frequencyMap.get(c));
         }
