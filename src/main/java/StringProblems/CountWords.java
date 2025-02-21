f77ad1aa2a9693ee65576cb6c77317b871dbe4f4
Kundan <kundank2p@gmail.com>
2025-02-21 11:22:46 +0530

    msg

--- src/main/java/StringProblems/CountWords.java
+++ src/main/java/StringProblems/CountWords.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 public class CountWords {
     public static void main(String[] args) {
-        String Str = "This is a input string for string input this is a stringx";
+        String Str = "This is a input string for string input this is a string";
        Map<String,Integer> map = new  HashMap<String,Integer>();
        int count = 1;
-       String array [] = Str.split(" ");
+       String[] array = Str.split(" ");
        for (int i = 0; i<array.length; i++){
            if (!map.containsKey(array[i])) {
                map.put(array[i], count);
@@ -24,30 +24,3 @@ public class CountWords {
     }
 
 
-
-
-
-//public class CountWords {
-//    public static void main(String[] args) {
-//        String str = "This is a input string used for string input input input input this is a string input";
-//
-//        // Convert to lowercase for case insensitivity
-//        str = str.toLowerCase();
-//
-//        // Use a HashMap to store word counts
-//        Map<String, Integer> map = new HashMap<>();
-//
-//        // Split the string into words
-//        String[] array = str.split("\\s+"); // Splitting based on whitespace
-//
-//        // Counting word occurrences
-//        for (String word : array) {
-//            map.put(word, map.getOrDefault(word, 0) + 1);
-//        }
-//
-//        // Printing the results
-//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-//            System.out.println("The count of the word '" + entry.getKey() + "' is " + entry.getValue());
-//        }
-//    }
-//}
